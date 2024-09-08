9233b2d79f0caf408e285469bd2b1fdb32714565
Caleb Johnson <caleb.johnson@ibm.com>
2024-09-08 18:38:47 -0500

    Create dev environments

--- setup.py
+++ setup.py
@@ -10,4 +10,21 @@ setup(
     package_data={"dice_solver": ["bin/Dice", "bin/*.so*"]},
     include_package_data=True,
     install_requires=["numpy", "pyscf"],
+    extras_require={
+        "dev": ["tox>=4.0", "pytest>=8.0"],
+        "docs": [
+            "qiskit-sphinx-theme~=2.0.0",
+            "sphinx-design",
+            "sphinx-autodoc-typehints",
+            "sphinx-copybutton",
+        ],
+        "style": [
+            "ruff==0.6.4",
+        ],
+        "lint": [
+            "mypy==1.11.2",
+            "pylint>=3.2.7",
+            "pydocstyle==6.3",
+        ],
+    },
 )
