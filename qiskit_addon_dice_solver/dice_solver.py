e3a47ccc51c3ffeaf876f3bd03f413d18a0662d1
Caleb Johnson <calebj1524@outlook.com>
2024-11-07 16:46:33 -0600

    Remove the solve_dice function (#43)
    * Remove solve_dice
    
    * Remove solve_dice
    
    * remove solve_dice from pkg?
    
    * style
    
    * reno
    
    * Lint
    
    * double reno

--- qiskit_addon_dice_solver/dice_solver.py
+++ qiskit_addon_dice_solver/dice_solver.py
@@ -142,7 +142,7 @@ def solve_hci(
     tools.fcidump.from_integrals(active_space_path, hcore, eri, norb, nelec)
 
     _write_input_files(
-        addresses=ci_strs,
+        ci_strs=ci_strs,
         active_space_path=active_space_path,
         norb=norb,
         num_up=n_alpha,
@@ -158,7 +158,7 @@ def solve_hci(
     _call_dice(dice_dir, mpirun_options)
 
     # Read and convert outputs
-    e_dice, sci_coefficients, addresses, avg_occupancies = _read_dice_outputs(
+    e_dice, sci_coefficients, sci_strs, avg_occupancies = _read_dice_outputs(
         dice_dir, norb
     )
 
@@ -169,7 +169,7 @@ def solve_hci(
     return (
         e_dice,
         sci_coefficients,
-        addresses,
+        sci_strs,
         (avg_occupancies[:norb], avg_occupancies[norb:]),
     )
 
@@ -262,109 +262,6 @@ def solve_fermion(
     return e_dice, sci_coefficients, avg_occupancies
 
 
-def solve_dice(
-    addresses: tuple[Sequence[int], Sequence[int]],
-    active_space_path: str | Path,
-    working_dir: str | Path,
-    spin_sq: float = 0.0,
-    *,
-    max_iter: int = 10,
-    clean_working_dir: bool = True,
-    mpirun_options: Sequence[str] | str | None = None,
-) -> tuple[float, np.ndarray, tuple[np.ndarray, np.ndarray]]:
-    """
-    Approximate the ground state given an active space and Slater determinant addresses.
-
-    In order to leverage the multi-processing nature of this tool, the user must specify
-    the CPU resources to use via the `mpirun_options` argument.
-
-    For example, to use 8 CPU slots in parallel in quiet mode:
-
-    .. code-block:: python
-
-       # Run 8 parallel slots in quiet mode
-       mpirun_opts = "-quiet -n 8"
-       # OR
-       mpirun_opts = ["-quiet", "-n", "8"]
-
-       energy, sci_coeffs, avg_occs = solve_dice(..., mpirun_options=mpirun_opts)
-
-    For more information on the ``mpirun`` command line options, refer to the `man page <https://www.open-mpi.org/doc/current/man1/mpirun.1.php>`_.
-
-    .. note::
-
-       Only closed-shell systems are currently supported. The particle number for both
-       spin-up and spin-down determinants is expected to be equal.
-
-    .. note::
-
-       Determinant addresses are interpreted by the ``Dice`` command line application as 5-byte unsigned integers; therefore, only systems
-       of ``40`` or fewer orbitals are supported.
-
-    Args:
-        addresses: A length-2 tuple of ``Sequence`` containing base-10, unsigned integer
-            representations of bitstrings. The first ``Sequence`` represents configurations of the alpha
-            particles, and the second ``Sequence`` represents that of the beta particles.
-        active_space_path: An absolute path to an FCI dump -- a format partially defined in
-            `Knowles and Handy 1989 <https://www.sciencedirect.com/science/article/abs/pii/0010465589900337?via%3Dihub>`_.
-        working_dir: An absolute path to a directory in which intermediate files can be written to and read from.
-        spin_sq: Target value for the total spin squared for the ground state. If ``None``, no spin will be imposed.
-        max_iter: The maximum number of HCI iterations to perform.
-        clean_working_dir: A flag indicating whether to remove the intermediate files used by the ``Dice``
-            command line application. If ``False``, the intermediate files will be left in a temporary directory in the
-            ``working_dir``.
-        mpirun_options: Options controlling the CPU resource allocation for the ``Dice`` command line application.
-            These command-line options will be passed directly to the ``mpirun`` command line application during
-            invocation of ``Dice``. These may be formatted as a ``Sequence`` of strings or a single string. If a ``Sequence``,
-            the elements will be combined into a single, space-delimited string and passed to
-            ``mpirun``. If the input is a single string, it will be passed to ``mpirun`` as-is. If no
-            ``mpirun_options`` are provided by the user, ``Dice`` will run on a single MPI slot. For more
-            information on the ``mpirun`` command line options, refer to the `man page <https://www.open-mpi.org/doc/current/man1/mpirun.1.php>`_.
-
-    Returns:
-        Minimum energy from SCI calculation, SCI coefficients, and average orbital occupancy for spin-up and spin-down orbitals
-    """
-    # Write Dice inputs to working dir
-    num_up = bin(addresses[0][0])[2:].count("1")
-    num_dn = bin(addresses[1][0])[2:].count("1")
-
-    intermediate_dir = Path(tempfile.mkdtemp(prefix="dice_cli_files_", dir=working_dir))
-
-    mf_as = tools.fcidump.to_scf(active_space_path)
-    num_orbitals = mf_as.get_hcore().shape[0]
-    _write_input_files(
-        addresses=addresses,
-        active_space_path=active_space_path,
-        norb=num_orbitals,
-        num_up=num_up,
-        num_dn=num_dn,
-        dice_dir=intermediate_dir,
-        spin_sq=spin_sq,
-        select_cutoff=1e12,
-        energy_tol=1e-10,
-        max_iter=max_iter,
-    )
-
-    # Navigate to working dir and call Dice
-    _call_dice(intermediate_dir, mpirun_options)
-
-    # Read outputs and convert outputs
-    e_dice, sci_coefficients, _, avg_occupancies = _read_dice_outputs(
-        intermediate_dir, num_orbitals
-    )
-    e_dice -= mf_as.mol.energy_nuc()
-
-    # Clean up the working directory of intermediate files, if desired
-    if clean_working_dir:
-        shutil.rmtree(intermediate_dir)
-
-    return (
-        e_dice,
-        sci_coefficients,
-        (avg_occupancies[:num_orbitals], avg_occupancies[num_orbitals:]),
-    )
-
-
 def _read_dice_outputs(
     dice_dir: str | Path, num_orbitals: int
 ) -> tuple[float, np.ndarray, tuple[np.ndarray, np.ndarray], np.ndarray]:
@@ -387,12 +284,12 @@ def _read_dice_outputs(
 
     # Construct the SCI wavefunction coefficients from Dice output dets.bin
     occs, amps = _read_wave_function_magnitudes(os.path.join(dice_dir, "dets.bin"))
-    addresses = _addresses_from_occupancies(occs)
-    sci_coefficients, addresses_a, addresses_b = (
-        _construct_ci_vec_from_addresses_amplitudes(amps, addresses)
+    ci_strs = _ci_strs_from_occupancies(occs)
+    sci_coefficients, ci_strs_a, ci_strs_b = _construct_ci_vec_from_amplitudes(
+        amps, ci_strs
     )
 
-    return energy_dice, sci_coefficients, (addresses_a, addresses_b), avg_occupancies
+    return energy_dice, sci_coefficients, (ci_strs_a, ci_strs_b), avg_occupancies
 
 
 def _call_dice(dice_dir: Path, mpirun_options: Sequence[str] | str | None) -> None:
@@ -424,7 +321,7 @@ def _call_dice(dice_dir: Path, mpirun_options: Sequence[str] | str | None) -> No
 
 
 def _write_input_files(
-    addresses: tuple[Sequence[int], Sequence[int]],
+    ci_strs: tuple[Sequence[int], Sequence[int]],
     active_space_path: str | Path,
     norb: int,
     num_up: int,
@@ -492,15 +389,15 @@ def _write_input_files(
     file1.close()
 
     ### Write the determinants to dice dir ###
-    up_addr, dn_addr = addresses
-    bytes_up = _address_list_to_bytes(up_addr)
-    bytes_dn = _address_list_to_bytes(dn_addr)
+    str_a, str_b = ci_strs
+    bytes_a = _ci_strs_to_bytes(str_a)
+    bytes_b = _ci_strs_to_bytes(str_b)
     file1 = open(os.path.join(dice_dir, "AlphaDets.bin"), "wb")  # type: ignore
-    for bytestring in bytes_up:
+    for bytestring in bytes_a:
         file1.write(bytestring)  # type: ignore
     file1.close()
     file1 = open(os.path.join(dice_dir, "BetaDets.bin"), "wb")  # type: ignore
-    for bytestring in bytes_dn:
+    for bytestring in bytes_b:
         file1.write(bytestring)  # type: ignore
     file1.close()
 
@@ -515,11 +412,11 @@ def _integer_to_bytes(n: int) -> bytes:
     return int(n).to_bytes(5, byteorder="big")
 
 
-def _address_list_to_bytes(addresses: Sequence[int]) -> list[bytes]:
-    """Convert a list of base-10 determinant addresses into a list of bytes."""
+def _ci_strs_to_bytes(ci_strs: Sequence[int]) -> list[bytes]:
+    """Convert a list of CI strings into a list of bytes."""
     byte_list = []
-    for address in addresses:
-        byte_list.append(_integer_to_bytes(address))
+    for ci_str in ci_strs:
+        byte_list.append(_integer_to_bytes(ci_str))
     return byte_list
 
 
@@ -567,42 +464,42 @@ def _bitstring_from_occupancy_str(occupancy_str: str) -> np.ndarray:
     return bitstring
 
 
-def _addresses_from_occupancies(occupancy_strs: list[str]) -> list[list[int]]:
-    """Convert occupancies to PySCF determinant addresses."""
+def _ci_strs_from_occupancies(occupancy_strs: list[str]) -> list[list[int]]:
+    """Convert occupancies to CI strings."""
     norb = len(occupancy_strs[0])
-    addresses = []
+    ci_strs = []
     for occ in occupancy_strs:
         bitstring = _bitstring_from_occupancy_str(occ)
         bitstring_a = bitstring[:norb]
         bitstring_b = bitstring[norb:]
-        address_a = sum(b << i for i, b in enumerate(bitstring_a))
-        address_b = sum(b << i for i, b in enumerate(bitstring_b))
-        address = [address_a, address_b]
-        addresses.append(address)
+        ci_str_a = sum(b << i for i, b in enumerate(bitstring_a))
+        ci_str_b = sum(b << i for i, b in enumerate(bitstring_b))
+        ci_str = [ci_str_a, ci_str_b]
+        ci_strs.append(ci_str)
 
-    return addresses
+    return ci_strs
 
 
-def _construct_ci_vec_from_addresses_amplitudes(
-    amps: list[float], addresses: list[list[int]]
+def _construct_ci_vec_from_amplitudes(
+    amps: list[float], ci_strs: list[list[int]]
 ) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
-    """Construct wavefunction amplitudes from determinant addresses and their associated amplitudes."""
-    uniques = np.unique(np.array(addresses))
+    """Construct wavefunction amplitudes from CI strings and their associated amplitudes."""
+    uniques = np.unique(np.array(ci_strs))
     num_dets = len(uniques)
     ci_vec = np.zeros((num_dets, num_dets))
-    addresses_a = np.zeros(num_dets, dtype=np.int64)
-    addresses_b = np.zeros(num_dets, dtype=np.int64)
+    ci_strs_a = np.zeros(num_dets, dtype=np.int64)
+    ci_strs_b = np.zeros(num_dets, dtype=np.int64)
 
-    addr_map = {uni_addr: i for i, uni_addr in enumerate(uniques)}
+    ci_str_map = {uni_str: i for i, uni_str in enumerate(uniques)}
 
-    for amp, address in zip(amps, addresses):
-        address_a, address_b = address
-        i = addr_map[address_a]
-        j = addr_map[address_b]
+    for amp, ci_str in zip(amps, ci_strs):
+        ci_str_a, ci_str_b = ci_str
+        i = ci_str_map[ci_str_a]
+        j = ci_str_map[ci_str_b]
 
         ci_vec[i, j] = amp
 
-        addresses_a[i] = uniques[i]
-        addresses_b[j] = uniques[j]
+        ci_strs_a[i] = uniques[i]
+        ci_strs_b[j] = uniques[j]
 
-    return ci_vec, addresses_a, addresses_b
+    return ci_vec, ci_strs_a, ci_strs_b
