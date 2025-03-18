3ecf0747d2461fc832f15e7b3d4563368472dcdd
Caleb Johnson <caleb.johnson@ibm.com>
2025-03-18 16:52:19 -0500

    Fix docstring

--- qiskit_addon_dice_solver/dice_solver.py
+++ qiskit_addon_dice_solver/dice_solver.py
@@ -201,17 +201,16 @@ def solve_fermion(
        of ``128`` or fewer orbitals are supported.
 
     Args:
-        bitstring_matrix: A set of configurations defining the subspace onto which the Hamiltonian
-            will be projected and diagonalized.
-
+        bitstring_matrix: A set of configurations defining the subspace onto which the Hamiltonian will be projected and diagonalized.
             This may be specified in two ways:
 
-            A bitstring matrix: A 2D ``numpy.ndarray`` of ``bool`` representations of bit values such that each row represents a single bitstring. The spin-up
+            Bitstring matrix: A 2D ``numpy.ndarray`` of ``bool`` representations of bit values such that each row represents a single bitstring. The spin-up
             configurations should be specified by column indices in range ``(N, N/2]``, and the spin-down configurations should be specified by column
             indices in range ``(N/2, 0]``, where ``N`` is the number of qubits.
 
             CI strings: A length-2 tuple of sequences containing integer representations of the spin-up and spin-down determinants, respectively.
-                The expected ordering is ``([a_str_0, ..., a_str_N], [b_str_0, ..., b_str_M])``.
+        
+            The expected ordering is ``([a_str_0, ..., a_str_N], [b_str_0, ..., b_str_M])``.
         hcore: Core Hamiltonian matrix representing single-electron integrals
         eri: Electronic repulsion integrals representing two-electron integrals
         open_shell: A flag specifying whether configurations from the left and right
