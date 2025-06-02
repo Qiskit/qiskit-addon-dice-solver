dafafc0063cf14cb5d9786587df5d52ee7de5309
Kevin J. Sung <kevjsung@umich.edu>
2025-06-02 11:15:24 -0400

    lint

--- qiskit_addon_dice_solver/dice_solver.py
+++ qiskit_addon_dice_solver/dice_solver.py
@@ -491,25 +491,23 @@ def _construct_ci_vec_from_amplitudes(
     amps: list[float], ci_strs: list[list[int]]
 ) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
     """Construct wavefunction amplitudes from CI strings and their associated amplitudes."""
-    ci_strs_a, ci_strs_b = zip(*ci_strs)
-    uniques_a = np.unique(ci_strs_a)
-    uniques_b = np.unique(ci_strs_b)
+    strs_a, strs_b = zip(*ci_strs)
+    uniques_a = np.unique(strs_a)
+    uniques_b = np.unique(strs_b)
     num_dets_a = len(uniques_a)
     num_dets_b = len(uniques_b)
+
     ci_vec = np.zeros((num_dets_a, num_dets_b))
     ci_strs_a = np.zeros(num_dets_a, dtype=np.int64)
     ci_strs_b = np.zeros(num_dets_b, dtype=np.int64)
 
-    ci_str_map_a = {uni_str: i for i, uni_str in enumerate(uniques_a)}
-    ci_str_map_b = {uni_str: i for i, uni_str in enumerate(uniques_b)}
+    ci_str_map_a = {s: i for i, s in enumerate(uniques_a)}
+    ci_str_map_b = {s: i for i, s in enumerate(uniques_b)}
 
-    for amp, ci_str in zip(amps, ci_strs):
-        ci_str_a, ci_str_b = ci_str
+    for amp, (ci_str_a, ci_str_b) in zip(amps, ci_strs):
         i = ci_str_map_a[ci_str_a]
         j = ci_str_map_b[ci_str_b]
-
         ci_vec[i, j] = amp
-
         ci_strs_a[i] = uniques_a[i]
         ci_strs_b[j] = uniques_b[j]
 
